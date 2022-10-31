2e60f0657f4c9459cbc1cf2f4cd61a54f19bd186
Nuttan Abhijan Swain <eng.nuttan@gmail.com>
2022-10-31 11:09:38 +0530

    Update TestInitializeHook.cs

--- BLKAutoFramework/Base/TestInitializeHook.cs
+++ BLKAutoFramework/Base/TestInitializeHook.cs
@@ -37,12 +37,11 @@ namespace BLKAutoFramework.Base
         {
             var chromeOption = new ChromeOptions();
             chromeOption.AddArguments(new List<string>() {
-                "--silent-launch",
-                "--no-startup-window",
-                "no-sandbox",
+                "--no-sandbox",
+                "--disable-dev-shm-usage",
                 "--headless",});
-            chromeOption.AddArguments("disable-infobars");
-            chromeOption.AddArguments("window-size=1920,1080");
+            //chromeOption.AddArguments("disable-infobars");
+            //chromeOption.AddArguments("window-size=1920,1080");
             new WebDriverManager.DriverManager().SetUpDriver(new ChromeConfig());
             _parallelConfig.Driver=new ChromeDriver(chromeOption);
 
