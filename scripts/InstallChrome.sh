e5f1ef76319b5584e736effcb36a482ef21016ea
Nuttan Abhijan Swain <eng.nuttan@gmail.com>
2024-02-27 13:13:10 +0530

    Update InstallChrome.sh

--- scripts/InstallChrome.sh
+++ scripts/InstallChrome.sh
@@ -2,4 +2,4 @@
 echo "chrome install success"
 set -ex
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-sudo apt install ./google-chrome-stable_current_amd64.deb
+sudo apt install -y ./google-chrome-stable_current_amd64.deb
